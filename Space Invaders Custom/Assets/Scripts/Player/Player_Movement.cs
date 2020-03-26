401bd5318a34a0f2a9e83464a860762ba624a24a
freercurse <jfreer84@googlemail.com>
2020-03-26 16:48:26 +0000

    movement big fixed

--- Space Invaders Custom/Assets/Scripts/Player/Player_Movement.cs
+++ Space Invaders Custom/Assets/Scripts/Player/Player_Movement.cs
@@ -6,6 +6,7 @@ public class Player_Movement : MonoBehaviour
 {
     public Rigidbody rb;
     [SerializeField] float thrust = 5f;
+    public int collided = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -25,7 +26,7 @@ public class Player_Movement : MonoBehaviour
         {
             rb.AddForce(-thrust, 0, 0);
         }
-        else if (Input.GetKeyUp(KeyCode.A))
+        else if (Input.GetKeyUp(KeyCode.A) && collided == 0)
         {
             rb.AddForce(thrust, 0, 0);
         }
@@ -34,10 +35,19 @@ public class Player_Movement : MonoBehaviour
         {
             rb.AddForce(thrust, 0, 0);
         }
-        else if (Input.GetKeyUp(KeyCode.D))
+        else if (Input.GetKeyUp(KeyCode.D) && collided == 0)
         {
             rb.AddForce(-thrust, 0, 0);
-        }
+        }       
+    }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        collided = 1;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        collided = 0;
     }
 }
