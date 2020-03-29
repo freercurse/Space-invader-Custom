f2fe8842b8d0f7d9bc81ee8952a0ef9fd7bac39f
freercurse <jfreer84@googlemail.com>
2020-03-30 00:42:21 +0100

    Set up additions to script to start counting points, scripts was attached to enemy, doesnt work so well, script gets reset with every destruction

--- Space Invaders Custom/Assets/Scripts/Enemy/EnemyGameplay.cs
+++ Space Invaders Custom/Assets/Scripts/Enemy/EnemyGameplay.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class EnemyGameplay : MonoBehaviour
 {
+    public int points = 0;
+    public GameObject pointsDisplay;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,11 +21,23 @@ public class EnemyGameplay : MonoBehaviour
        
     }
 
-    void OnCollisionEnter(Collision collision)
+    public void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Projectile")
         {
             DestroyObject(this.gameObject);
+
+            points = points + 2;
+            setScore();
         }
+    }
+
+    public void setScore()
+    {
+        Text pointsToDisplay = pointsDisplay.GetComponent<Text>();
+        pointsToDisplay.text = points.ToString();
+        Debug.Log(points.ToString());
+        Debug.Log("points have been scored");
+       
     }
 }
