90deb6c560bfa30bbbd15aee2d4629f8bb0c92a3
freercurse <jfreer84@googlemail.com>
2020-03-29 23:13:45 +0100

    Fixed projectile collision bug, limited fire rate, and enable enemy collision and death

--- Space Invaders Custom/Assets/Scripts/Player/Player_Shoot.cs
+++ Space Invaders Custom/Assets/Scripts/Player/Player_Shoot.cs
@@ -2,11 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 public class Player_Shoot : MonoBehaviour
 {
     public GameObject Lazer;
     public float shootForce;
+    public float timeBeforeShot = 0.4f;
+    bool timer = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +21,23 @@ public class Player_Shoot : MonoBehaviour
     void Update()
     {
         Shoot();
+
+    }
+
+   IEnumerator Timer ()
+    {
+        yield return new WaitForSeconds(timeBeforeShot);
+        timer = true;
     }
 
     private void Shoot()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && timer == true)
         {
             GameObject shot = GameObject.Instantiate(Lazer, transform.position, transform.rotation);
             shot.GetComponent<Rigidbody>().AddForce(transform.up * shootForce);
+            timer = false;
+            StartCoroutine(Timer());
         }
     }
 }
